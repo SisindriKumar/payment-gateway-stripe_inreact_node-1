5943b68cbea4f5f6f1d1fa526e3f0f90ac7375fe
SisindriKumar <mail.sisindrikumarreddy@gmail.com>
2024-02-16 16:28:04 +0530

    first commit

---
+++ client/src/components/Sucess.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Success.css"; // Import CSS file for styling
+
+const Success = () => {
+  return (
+    <div className="success-container">
+      <div className="success-message">Your payment has been succeeded.</div>
+    </div>
+  );
+};
+
+export default Success;
